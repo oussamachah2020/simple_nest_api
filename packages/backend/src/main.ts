1e325a4922b26a371613a012fa3325d045cefaea
Oussama CHAHIDI <oussama@ismaeland.com>
2023-09-29 18:08:14 +0100

    feat(): adding swagger doc

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -7,12 +7,22 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+  const config = new DocumentBuilder()
+    .setTitle('CRUD NEST API')
+    .setDescription('simple nest api with crud func')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
